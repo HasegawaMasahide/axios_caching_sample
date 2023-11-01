f7969c4d1d7e08c65f767f8674bf5c9cda99ed19
h_masahide <hasegawa_masahide@nanairo-inc.jp>
2023-11-01 17:50:41 +0900

    キャッシュ保持期間を１０秒に設定

--- src/clients.ts
+++ src/clients.ts
@@ -1,12 +1,14 @@
-import axios, { AxiosAdapter } from "axios";
+import axios, { AxiosAdapter, AxiosPromise } from "axios";
 import { setup } from "axios-cache-adapter";
-import { cacheAdapterEnhancer } from "axios-extensions";
+import { ICacheLike, cacheAdapterEnhancer } from "axios-extensions";
+import { CACHE_MAX_AGE_SEC } from "./constant";
+import LRUCache from "lru-cache";
 
 const with_no_cache = axios.create();
 
 const with_adapter = setup({
 	cache: {
-		maxAge: 5 * 60 * 1000,	// ms
+		maxAge: CACHE_MAX_AGE_SEC * 1000,	// ms
 		debug: true,
 		exclude: {
 			query: false
@@ -14,8 +16,17 @@ const with_adapter = setup({
 	}
 });
 
+const cacheLike: ICacheLike<AxiosPromise<any>> = new LRUCache({
+	ttl: CACHE_MAX_AGE_SEC * 1000, 	// ms
+	max: 100
+});
 const with_extensions = axios.create({
-	adapter: cacheAdapterEnhancer(axios.defaults.adapter as AxiosAdapter)
+	adapter: cacheAdapterEnhancer(
+		axios.defaults.adapter as AxiosAdapter,
+		{
+			defaultCache: cacheLike
+		}
+	)
 });
 
 export { with_no_cache, with_adapter, with_extensions };
